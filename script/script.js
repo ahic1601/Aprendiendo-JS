bafc853adc97da33a0bb4ce026a4ad479fd0b0ca
Alejandro Iglesias <ahic1601@gmail.com>
2022-12-29 18:12:03 -0300

    practicando

--- script/script.js
+++ script/script.js
@@ -1,194 +1,253 @@
-//  Farmacia JS
+// CODER E-COMMERCE ASYNC
+const token = 'TEST-7958689689200934-021122-401c5047ace73e3c4c46253f6c18a3e3-31337594';
+
+const precioDolarText = document.getElementById('precio-dolar');
+const modalCompra = new bootstrap.Modal(document.getElementById('modal-compra'))
+const modalTitle = document.getElementById('modal-title');
+const modalBody = document.getElementById('modal-body');
+const btnWeb = document.getElementById('btn-web');
+const btnPagar = document.getElementById('btn-pagar');
+
+const JSONResponse = async (data) => {
+    const response = await data;
+    return await response.json();
+}
+
 
 const spinner = `<div class="d-flex justify-content-center m-auto">
                     <div class="spinner-border" role="status">
                         <span class="visually-hidden">Loading...</span>
                     </div>
-                </div>`;
+                </div>`
 
-const farmacos = [
-    {
-        id: 1,
-        name: 'Diclofenac Sodico',
-        comercialName: 'Rodinac 75',
-        miligram: 75,
-        amount: 15,
-        box: 1,
-        price: 800,
-        photo: 'https://geminisfarmaceutica.com.ar/wp-content/uploads/2016/09/productos31.jpg',
-    },
-    {
-        id: 2,
-        name: 'Diclofenac Sodico',
-        comercialName: 'Diclogesic 75',
-        miligram: 75,
-        amount: 30,
-        box: 1,
-        price: 1600,
-        photo: 'https://trbpharma.com/_site/wp-content/uploads/2020/12/23.-Diclogesic75x10-2650-05-110x57-copia.jpg',
-    },
-    {
-        id: 3,
-        name: 'Diclofenac Potasico',
-        comercialName: 'Blokium P 75',
-        miligram: 75,
-        amount: 10,
-        box: 1,
-        price: 1200,
-        photo: 'https://www.casasco.com.ar/wp-content/uploads/2018/04/Pack_web-24_blokiumP.jpg',
-    },
-    {
-        id: 4,
-        name: 'Ibuprofeno',
-        comercialName: 'Ibupirac Fast',
-        miligram: 400,
-        amount: 10,
-        box: 1,
-        price: 600,
-        photo: 'https://www.ibupirac.com.uy/public/files/novedades/ibupirac_adaptaciones_1_60b77e02278a1.png',
-    },
-    {
-        id: 5,
-        name: 'Paracetamol',
-        comercialName: 'Tafirol',
-        miligram: 500,
-        amount: 30,
-        box: 1,
-        price: 800,
-        photo: 'http://d3ugyf2ht6aenh.cloudfront.net/stores/001/671/329/products/24-21-5447cf43590d84c30016262057208643-640-0.png',
-    },
-];
+const callML = () => {
+    return fetch('https://api.mercadolibre.com/sites/MLA/search?category=MLA1055');
+}
 
+const precioDolar = fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales');
 
-let contenedor = document.getElementById ('contenedor');
-const inputSearch = document.getElementById('input-search');
-let contenedorCarrito = document.getElementById('contenedor-carrito');
-let contenedorCarritoTotal = document.getElementById('contenedor-carrito-total')
+// const traerDolar = () => {
+//     precioDolar
+//     .then(res => res.json())
+//     .then(res => {
+//         console.log(res);
+//         const oficial = res.find(dolar => dolar.casa.agencia === '349');
+//         console.log(oficial);
+//         precioDolarText.innerText = `Dolar Oficial:  Compra $${oficial?.casa.compra} - Venta $${oficial?.casa.venta}`
+//     });
+// }
 
+const traerDolar = async () => {
+    const respuesta = await JSONResponse(precioDolar);
+    console.log(respuesta);
+    const oficial = respuesta.find(dolar => dolar.casa.agencia === '349');
+    precioDolarText.innerText = `Dolar Oficial:  Compra $${oficial?.casa.compra} - Venta $${oficial?.casa.venta}`
 
-const mostrarFarmacosHtml = (farmacos, contenedor) => {
-    let acumulador = '';
-    farmacos.forEach(element => {
-        acumulador += `
-        <div class="card" style="width: 18rem;">
-            <img src="${element.photo}" class="card-img-top" alt="${element.comercialname}">
-            <div class="card-body">
-                <h5 class="card-title">${element.comercialName}</h5>
-                <p class="card-text">${element.name +' '+ element.miligram + 'mg x ' + element.amount + ' comprimidos.'}</p>
-                <p class="card-text">Cajas X ${element.box}</p>
-                <p class="card-text">Precio: ${'$ ' + element.price}</p>
-                <a href="#" onclick="agregarAlCarrito(${element.id})" class="btn btn-primary">Comprar</a>
-            </div>
-        </div>
-        `
+}
+
+const fetchML = (item) => fetch('https://api.mercadopago.com/checkout/preferences', {
+    headers: {
+        "Authorization": 'Bearer ' + token,
+        "Content-Type": "application/json"
+    },
+    method: 'POST',
+    body: JSON.stringify({
+            "items": [
+       item
+          ]
     })
-    contenedor.innerHTML = acumulador;
+})
+
+const pagoML = async (item) => {
+    // const response = await fetchML();
+    // const responseJSON = await response.json()
+    const response = await JSONResponse(fetchML(item))
+
+    console.log(response);
+    //REDIRIGIMOS A LA URL DE PAGO
+    window.location.href = response.init_point
 }
 
 
-const database = new Promise (res => {
-    setTimeout (()=>{
-        res(farmacos)
-    }, 3000)
-})
+
+const irAlPago = (e) => {
+    const id = e.target.getAttribute('ref');
+
+    const producto = productos.find(producto => producto.id === id);
+
+    if (!producto) {
+        return;
+    }
+
+    const item = {
+        title: producto.title,
+        description: producto.title,
+        picture_url: producto.thumbnail,
+        category_id: producto.category_id,
+        quantity: 1,
+        currency_id: "ARS",
+        unit_price: producto.price
+      }
+      btnPagar.innerHTML = spinner;
+      pagoML(item)
+    
+}
+
 
 let carrito = [];
 let productos = [];
 
 const getInfo = () => {
     contenedor.innerHTML = spinner;
-    
-    database
-    .then ((res) => {
+
+    callML()
+    .then(res => res.json())
+    .then((res) => {
         console.log(res);
-        productos = res;
-        mostrarFarmacosHtml(res, contenedor)
+        productos = [...productos, ...res.results];
+        dibujarProductos(productos, contenedor)
     })
-    .finally(() => {
+    .finally(() =>{
         console.log('termino de cargar')
     })
 }
 
-
 class Carrito {
-    constructor(id, comercialName, box, price, photo) {
+    constructor(id, nombre, cantidad, precio, imagen) {
         this.id = id;
-        this.comercialName = comercialName;
-        this.box = box;
-        this.price = price;
-        this.photo = photo;
-        this.total = price * box;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.imagen = imagen;
+        this.total = precio * cantidad;
     }
 }
+
+const contenedor = document.getElementById('contenedor');
+const inputSearch = document.getElementById('input-search');
+const contenedorCarrito = document.getElementById('contenedor-carrito');
 
 
 const agregarAlCarrito = (id) => {
     if (!id) {
         return;
     }
-    let producto = farmacos.find(el => el.id === id);
+    const producto = productos.find(el => el.id === id);
+
     if (producto) {
-        const productoCarrito = new Carrito (producto.id, producto.comercialName, producto.box, producto.price, producto.photo);
+        const productoCarrito = new Carrito(producto.id, producto.nombre, 1, producto.precio, producto.imagen);
+
         if (carrito.some(el => el.id === id)) {
             const target = carrito.find(el => el.id === id);
             carrito = carrito.filter(el => el.id !== id);
-            const nuevoProducto = new Carrito (target.id, target.comercialName, target.box + 1, target.price, target.photo);
-            carrito.push(nuevoProducto);
-        }else{
+
+            const nuevoProducto = new Carrito(target.id, target.nombre, target.cantidad + 1, target.precio, target.imagen);
+            carrito.push(nuevoProducto)
+        } else {
             carrito.push(productoCarrito);
         }
+
     }
-    localStorage.setItem('carrito', JSON.stringify(carrito))
-    listarCarrito(carrito,contenedorCarrito);
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+    listarCarrito(carrito)
 }
 
-const listarCarrito = (productoCarrito, contenedorCarrito) => {
-    let acumuLador ='';
-    productoCarrito.forEach((producto) => {
-        acumuLador += `
+const listarCarrito = (productosCarrito) => {
+    let acumulador = '';
+
+    productosCarrito.forEach((producto) => {
+        acumulador += `
         <tr>
-            <th scope="row">${producto.id}</th>
-            <td>${producto.comercialName}</td>
-            <td>$${producto.price}</td>
-            <td>${producto.box}</td>
+        <th scope="row">${producto.id}</th>
+            <td>${producto.nombre}</td>
+            <td>$${producto.precio}</td>
+            <td>${producto.cantidad}</td>
             <td>$${producto.total}</td>
         </tr>
         `
     })
-    contenedorCarrito.innerHTML = acumuLador;
+    contenedorCarrito.innerHTML = acumulador;
 }
 
-const totalCarrito = (productoCarrito, contenedorCarritoTotal) => {
-    let acumuLador ='';
-    productoCarrito.forEach((producto) => {
-        acumuLador += `
-        <tr>
-            <th scope="row"></th>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>$${producto.total}</td>
-        </tr>
+const dibujarProductos = (productos, contenedor) => {
+    let acumulador = '';
+
+    productos.forEach(element => {
+        acumulador += `
+        <div class="card" style="width: 18rem;">
+            <img src="${element.thumbnail}" class="card-img-top" alt="${element.title}.">
+            <div class="card-body">
+                <h5 class="card-title">${element.title}</h5>
+                <p class="card-text">Cantidad: ${element.available_quantity}</p>
+                <p class="card-text">Precio: $ ${element.price.toLocaleString('es', {
+                    minimumFractionDigits: 2, maximumFractionDigits: 2
+                })}</p>
+                <a href="#" onclick="mostrarModal('${element.id}')" class="btn btn-primary">Comprar</a>
+            </div>
+        </div>
         `
-    })
-    contenedorCarritoTotal.innerHTML = acumuLador;
+    });
+    contenedor.innerHTML = acumulador;
+}
+
+const mostrarModal = (id) =>{
+    const producto = productos.find(producto => producto.id === id);
+
+    if (!producto) {
+        return;
+    }
+
+    modalTitle.innerText = producto.title;
+    modalBody.innerHTML = `
+        <div class="d-flex flex-row w-100">
+            <div>
+            <img src="${producto.thumbnail}" class="card-img-top" alt="${producto.title}.">
+            </div>
+            <div>
+            <h5 class="card-title">${producto.title}</h5>
+                <p class="card-text">Cantidad: ${producto.available_quantity}</p>
+                <p class="card-text">Precio: $ ${producto.price.toLocaleString('es', {
+                    minimumFractionDigits: 2, maximumFractionDigits: 2
+                })}</p>
+            </div>
+        </div>
+    `
+    btnWeb.href = producto.permalink;
+
+    btnPagar.setAttribute('ref', id);
+
+    modalCompra.show();
 }
 
 
 const handleSearch = (e) => {
     console.log(e.target.value);
 
-    const filtrarFarmacos = farmacos.filter(element => element.name.toLowerCase().includes(e.target.value.toLowerCase()));
+    const filtrados = productos.filter(producto => producto.title.toLocaleLowerCase().includes(e.target.value.toLowerCase()))
 
-    mostrarFarmacosHtml(filtrarFarmacos, contenedor);
-};
+    dibujarProductos(filtrados, contenedor)
+} 
 
-if(localStorage.getItem('carrito')){
+if (localStorage.getItem('carrito')) {
     carrito = JSON.parse(localStorage.getItem('carrito'));
-    listarCarrito(carrito, contenedorCarrito)
+    listarCarrito(carrito)
+
 }
 
 getInfo();
+traerDolar();
 
-inputSearch.addEventListener ('input', handleSearch);
+inputSearch.addEventListener('input', handleSearch);
+btnPagar.addEventListener('click', irAlPago);
 
+const infoML = fetch('https://api.mercadolibre.com/sites/MLA/search?category=MLA1055');
+console.log(infoML);
+
+infoML.then((res) => 
+   res.json()
+)
+.then(res => console.log(res))
+
+// fetch('./script/productos.json')
+// .then(res => res.json())
+// .then(res => console.log(res));
